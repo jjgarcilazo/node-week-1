c5c840051a21d14c18779bf9110d2d28972cc789
Jesse  Garcilazo <jjgarcilazo@gmail.com>
2022-02-04 17:13:23 -0800

    Passport

--- Desktop/NucampFolder/5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ Desktop/NucampFolder/5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -5,15 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
-
 const mongoose = require('mongoose');
-
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
@@ -21,91 +21,60 @@ const connect = mongoose.connect(url, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 });
-
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err)
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
 );
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-  function auth(req, res, next) {
-    app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
-    app.use(session({
-      name: 'session-id',
-      secret: '12345-67890-09876-54321',
-      saveUninitialized: false,
-      resave: false,
-      store: new FileStore()
-  }));
-  function auth(req, res, next) {
-      console.log(req.session);
-  
-      if (!req.session.user) {
-        app.use('/', indexRouter);
-        app.use('/users', usersRouter);
-        
-        function auth(req, res, next) {
-            console.log(req.session);
-        
-            if (!req.session.user) {
-                const err = new Error('You are not authenticated!');
-                err.status = 401;
-                return next(err);
-            } else {
-                if (req.session.user === 'authenticated') {
-                    return next();
-                } else {
-                    const err = new Error('You are not authenticated!');
-                    err.status = 401;
-                    return next(err);
-                }
-            }
-        }
-          }
-      } else {
-        if (req.session.user === 'authenticated') {
-              return next();
-          } else {
-              const err = new Error('You are not authenticated!');
-              err.status = 401;
-              return next(err);
-          }
-      }
-  }
- app.use(auth);
-
-app.use(auth);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    const err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
+  }
+  else {
+    return next();
+  }
+};
+app.use(auth);
+ 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
